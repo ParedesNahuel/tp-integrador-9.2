dad240500f07ccdc66115401ef3257cf3c9255a6
ParedesNahuel <81544925+ParedesNahuel@users.noreply.github.com>
2022-02-14 21:57:44 -0300

    Anda el filtrado de reportes

--- tp integrador/ListaFormulariosPorGestores.aspx
+++ tp integrador/ListaFormulariosPorGestores.aspx
@@ -13,6 +13,30 @@
         }
         .auto-style2 {
             height: 230px;
+            width: 271px;
+        }
+        .auto-style5 {
+            height: 230px;
+            width: 228px;
+        }
+        .auto-style6 {
+            width: 228px;
+        }
+        .auto-style7 {
+            width: 271px;
+        }
+        .auto-style8 {
+            height: 230px;
+            width: 469px;
+        }
+        .auto-style10 {
+            width: 318px;
+        }
+        .auto-style11 {
+            width: 298px;
+        }
+        .auto-style12 {
+            width: 469px;
         }
     </style>
 </head>
@@ -21,38 +45,79 @@
         <div>
             <table class="auto-style1">
                 <tr>
-                    <td class="auto-style2"></td>
-                    <td class="auto-style2">
-                        <h1>FORMULARIOS POR GESTOR</h1>
+                    <td class="auto-style5">
+                        <asp:LinkButton ID="lbtnMaster" runat="server" PostBackUrl="~/MenuGestoriaMaster.aspx">Volver al Menu anterior</asp:LinkButton>
                     </td>
-                    <td class="auto-style2">
+                    <td class="auto-style8">
+                        <h1>FORMULARIOS POR GESTOR</h1>
+                    </td class="auto-style2">
+                    <td>
+
+                    </td>
+                    <td >
                         <asp:Image ID="Image1" runat="server" Height="250px" ImageUrl="~/Imagenes/logo.auto1.jpeg" Width="250px" />
                     </td>
                 </tr>
+               
                 <tr>
-                    <td>&nbsp;</td>
-                    <td>&nbsp;</td>
-                    <td>&nbsp;</td>
+                    <td class="auto-style6">
+                       
+                        Marca
+                        <br />
+                        <br />
+                         <asp:DropDownList ID="ddlMarca" runat="server">
+                        </asp:DropDownList>
+                    </td>
+                    <td class="auto-style12">
+                         Gamma
+                        <br />
+                        <br />
+                        <asp:DropDownList ID="ddlGamma" runat="server">
+                        </asp:DropDownList>
+
+                    </td>
+                    <td class="auto-style7">
+                          Modelo
+                        <br />
+                        <br />
+                        <asp:DropDownList ID="ddlModelo" runat="server">
+                        </asp:DropDownList>
+
+                    </td>
+                    <td class="auto-style11">
+                          Gestor
+                        <br />
+                        <br />
+                        <asp:DropDownList ID="ddlGestor" runat="server">
+                        </asp:DropDownList>
+
+                    </td>
+                    <td class="auto-style10">
+                          Cliente
+                        <br />
+                        <br />
+                        <asp:DropDownList ID="ddlCliente" runat="server">
+                        </asp:DropDownList>
+                    </td>
+                    <td>
+                       
+                     <asp:Button runat ="server" ID="btnFiltrar" Text="Filtrar" OnClick="btnFiltrar_Click" />
+                    </td>
+                   
                 </tr>
                 <tr>
-                    <td>&nbsp;</td>
-                    <td>
-                        <asp:GridView ID="GridView1" runat="server" DataSourceID="SqlDataSource1">
+                    <td class="auto-style6">
+                        &nbsp;</td>
+                    <td class="auto-style12">
+                        <asp:GridView ID="grdvwFormularios" runat="server">
                         </asp:GridView>
                     </td>
-                    <td>&nbsp;</td>
+                    <td class="auto-style7">&nbsp;</td>
                 </tr>
                 <tr>
-                    <td>
-                        <asp:SqlDataSource ID="SqlDataSource1" runat="server"></asp:SqlDataSource>
-                    </td>
-                    <td>&nbsp;</td>
-                    <td>&nbsp;</td>
-                </tr>
-                <tr>
-                    <td>&nbsp;</td>
-                    <td>&nbsp;</td>
-                    <td>&nbsp;</td>
+                    <td class="auto-style6">&nbsp;</td>
+                    <td class="auto-style12">&nbsp;</td>
+                    <td class="auto-style7">&nbsp;</td>
                 </tr>
             </table>
         </div>
--- tp integrador/ListaFormulariosPorGestores.aspx.cs
+++ tp integrador/ListaFormulariosPorGestores.aspx.cs
@@ -4,13 +4,134 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Data;
+using Entidades;
+using Negocio;
 
 namespace tp_integrador
 {
     public partial class ListaFormulariosPorGestores : System.Web.UI.Page
     {
+        public NegocioFormularios negForm= new NegocioFormularios();
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (!IsPostBack)
+            {
+                cargarMarcas();
+                cargarGammas();
+                cargarModelos();
+                cargarGestores();
+                cargarClientes();
+                DataTable dt = negForm.GetFiltrado("","","","","");
+                grdvwFormularios.DataSource = dt;
+                grdvwFormularios.DataBind();
+            }
+        }
+
+        private void cargarClientes()
+        {
+            NegocioClientes negCliente = new NegocioClientes();
+            DataTable Dt = negCliente.getTabla();
+            DataRow dr = Dt.NewRow();
+            dr["dni_cl"] = 0;
+            dr["nombre_cl"] = "todos";
+            dr["apellido_cl"] = "";
+            //dr["NombreCompleto"] = "Todos";
+            Dt.Rows.InsertAt(dr, 0);
+            Dt.Columns.Add("NombreCompleto",typeof(string),"nombre_cl+ ' ' +apellido_cl");
+            ddlCliente.DataSource = Dt;
+            ddlCliente.DataValueField = "dni_cl";
+            ddlCliente.DataTextField = "NombreCompleto";
+            ddlCliente.DataBind();
+            ddlCliente.SelectedValue = "0";
+        }
+
+        private void cargarGestores()
+        {
+            negociogestores negGestor = new negociogestores();
+            DataTable dataTable = negGestor.getTabla();
+            DataRow dr = dataTable.NewRow();
+            dr["matricula_ge"] = 0;
+            dr["nombre_ge"] = "todos";
+            dr["apellido_ge"] = "";
+            //dr["NombreCompleto"] = "Todos";
+            dataTable.Rows.InsertAt(dr, 0);
+            dataTable.Columns.Add("NombreCompleto", typeof(string), "nombre_ge+ ' ' +apellido_ge");
+            ddlGestor.DataSource = dataTable;
+            ddlGestor.DataValueField = "matricula_ge";
+            ddlGestor.DataTextField = "NombreCompleto";
+            
+            ddlGestor.DataBind();
+            ddlGestor.SelectedValue = "0";
+        }
+
+        private void cargarModelos()
+        {
+            NegocioModelos negModelo = new NegocioModelos();
+            DataTable dataTable = negModelo.getTabla();
+            DataRow dr = dataTable.NewRow();
+            dr["nombre_mo"] = "Todos";
+            dr["cod_mo"] = 0;
+            dataTable.Rows.InsertAt(dr, 0);
+            ddlModelo.DataSource = dataTable;
+            ddlModelo.DataValueField = "cod_mo";
+            ddlModelo.DataTextField = "nombre_mo";
+            ddlModelo.DataBind();
+            ddlModelo.SelectedValue = "0";
+        }
+
+        private void cargarGammas()
+        {
+            NegocioGamma negMarca= new NegocioGamma();
+            DataTable dataTable = negMarca.getTabla();
+            DataRow dr = dataTable.NewRow();
+            dr["nombre_ga"] = "Todos";
+            dr["cod_ga"] = 0;
+            dataTable.Rows.InsertAt(dr, 0);
+            ddlGamma.DataSource = dataTable;
+            ddlGamma.DataValueField = "cod_ga";
+            ddlGamma.DataTextField = "nombre_ga";
+            ddlGamma.DataBind();
+            ddlGamma.SelectedValue = "0";
+        }
+
+        private void cargarMarcas()
+        {
+            NegocioMarcas negMarca = new NegocioMarcas();
+            DataTable dataTable = negMarca.getTabla();
+            DataRow dr = dataTable.NewRow();
+            dr["nombre_ma"] = "Todos";
+            dr["cod_ma"] = 0;
+            dataTable.Rows.InsertAt(dr, 0);
+            ddlMarca.DataSource = dataTable;
+            ddlMarca.DataValueField = "cod_ma";
+            ddlMarca.DataTextField = "nombre_ma";
+            ddlMarca.DataBind();
+            ddlMarca.SelectedValue = "0";
+        }
+
+        protected void btnFiltrar_Click(object sender, EventArgs e)
+        {
+            string codMarca = ddlMarca.SelectedValue;
+            string codGamma = ddlGamma.SelectedValue;
+            string codModelo = ddlModelo.SelectedValue;
+            string codCliente = ddlCliente.SelectedValue;
+            string codGestor = ddlGestor.SelectedValue;
+
+            if (codMarca == "0")
+                codMarca = "";
+            if (codGamma == "0")
+                codGamma = "";
+            if (codModelo == "0")
+                codModelo = "";
+            if (codCliente == "0")
+                codCliente = "";
+            if (codGestor == "0")
+                codGestor = "";
+
+           DataTable dt = negForm.GetFiltrado(codMarca,codGamma,codModelo,codCliente,codGestor);
+            grdvwFormularios.DataSource = dt;
+            grdvwFormularios.DataBind();
 
         }
     }
