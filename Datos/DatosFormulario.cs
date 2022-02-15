ffe2ec0d01b2ed8f4df1433f64295ce142c0298c
Bruno-90 <67373365+Bruno-90@users.noreply.github.com>
2022-02-15 20:34:14 -0300

    Update DatosFormulario.cs
    TIENE ALGUNAS MODIFICACIONES PARA QUE FUNCIONE EL CARGAR FORMULARIO
    DEJO EL PROCEDIMIENTO PARA AGREGAR LOS DATOS
    
    CREATE PROCEDURE [dbo].[spAgregarFormulario]
            (
    
    @Patente varchar(20),
    @Matricula varchar(20),
    @Dni_cli varchar(20),
    @Nombre_cony varchar(20),
    @Apellido_cony varchar(20),
    @Personeria varchar(20)
    
    
            )
    		
            AS
            INSERT INTO Formularios (patente_fo,matricula_gestor_fo,dni_cliente_fo,nombre_conyu_fo,apellido_conyu_fo,personeria_fo) VALUES(@Patente,@Matricula,@Dni_cli,@Nombre_cony,@Apellido_cony,@Personeria)
           RETURN

--- Datos/DatosFormulario.cs
+++ Datos/DatosFormulario.cs
@@ -197,3 +197,130 @@ namespace Datos
         }
     }
 }
+
+
+
+
+/////////////////////////////////////////////////NUEVOOOOOOOOOOOOOO
+
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entidades;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Datos
+{
+    public class DatosFormulario
+    {
+        AccesoDatos ds = new AccesoDatos();
+
+
+        public Formularios getFormularios(Formularios formu)
+
+        { //creo que la busqueda de la tabla debe ser por la patente, ya que el codigo de registro es autonumerico
+
+            DataTable tabla = ds.obtenerTabla("Formularios", "Select * from Formularios where patente_fo = '" + formu.GetPatenteFO() + "'");
+
+            formu.SetCodigoFO(Convert.ToInt32(tabla.Rows[0][0].ToString()));
+            formu.SetPatenteFO(tabla.Rows[0][1].ToString());
+            formu.SetMatGestorFO(tabla.Rows[0][2].ToString());
+            formu.SetDniCliFO(tabla.Rows[0][3].ToString());           
+            formu.SetNombConyuFO(tabla.Rows[0][4].ToString());
+            formu.SetApeConyuFO(tabla.Rows[0][5].ToString());
+            formu.SetPersoneriaFO(tabla.Rows[0][6].ToString());
+            return formu;
+
+        }
+
+
+        public Boolean existeformulario(Formularios formu)
+        {
+            string consulta = "select cod_registro_fo from formularios where cod_registro_fo='" + formu.GetCodigoFO() + "'";
+            return ds.existe(consulta);
+        }
+        public DataTable GetTablaFormularios()
+        {
+
+            DataTable tabla = ds.obtenerTabla("Formularios", "Select * from Formularios");
+            return tabla;
+        }
+
+        //---------------no creo que se use el eliminar formularios, pero aqui lo dejo por las dudas------------------
+
+        /*public int EliminarFormularios(Formularios formu)
+        {
+            SqlCommand comando = new SqlCommand();
+            ArmarParametrosFormulariosEliminar(ref comando, formu);
+            return ds.EjecutarProcedimientoAlmacenado(comando, "spEliminarFormularios");
+        }*/
+        private void ArmarParametrosFormulariosEliminar(ref SqlCommand Comando, Formularios formu)
+        {
+            SqlParameter SqlParametros = new SqlParameter();
+            SqlParametros = Comando.Parameters.Add("@CODIGO", SqlDbType.Int);
+            SqlParametros.Value = formu.GetCodigoFO();
+        }
+        public int AgregarFormularios(Formularios formu)
+        {
+            SqlCommand comando = new SqlCommand();
+            ArmarParametrosFormulariosAgregar(ref comando, formu);
+            return ds.EjecutarProcedimientoAlmacenado(comando, "spAgregarFormulario");
+        }
+
+        //---------------no creo que se use el actualizar o editar de formularios, pero aqui lo dejo por las dudas------------------
+
+        /*public int ActualizarFormulariosDAO(Formularios formu)
+        {
+            SqlCommand comando = new SqlCommand();
+            ArmarParametrosFormulariosEditar(ref comando, Formularios);
+
+            return ds.EjecutarProcedimientoAlmacenado(comando, "spEditarFormularios");
+
+        }
+
+        private void ArmarParametrosFormulariosEditar(ref SqlCommand comando, Formularios formu)
+        {
+            SqlParameter parametro = new SqlParameter();
+
+            parametro = comando.Parameters.Add("@COD_REGISTRO_fo", SqlDbType.Int);
+            parametro.Value = formu.GetCodigoFO();
+            parametro = comando.Parameters.Add("@PATENTE_fo", SqlDbType.VarChar);
+            parametro.Value = formu.GetPatenteFO();
+            parametro = comando.Parameters.Add("@MATRICULA_GESTOR_fo", SqlDbType.VarChar);
+            parametro.Value = formu.GetMatGestorFO();
+            parametro = comando.Parameters.Add("@DNI_CLIENTE_fo", SqlDbType.VarChar);
+            parametro.Value = formu.GetDniCliFO();
+            parametro = comando.Parameters.Add("@NOMBRE_CONYU_fo", SqlDbType.VarChar);
+            parametro.Value = formu.GetNombConyuFO();
+            parametro = comando.Parameters.Add("@APELLIDO_CONYU_fo", SqlDbType.VarChar);
+            parametro.Value = formu.GetApeConyuFO();
+            parametro = comando.Parameters.Add("@PERSONERIA_fo", SqlDbType.VarChar);
+            parametro.Value = formu.GetPersoneriaFO();
+
+        }*/
+
+        private void ArmarParametrosFormulariosAgregar(ref SqlCommand Comando, Formularios formu)
+        {
+            SqlParameter SqlParametros = new SqlParameter();
+           
+            SqlParametros = Comando.Parameters.Add("@Patente", SqlDbType.VarChar);
+            SqlParametros.Value = formu.GetPatenteFO();
+            SqlParametros = Comando.Parameters.Add("@Matricula", SqlDbType.VarChar);
+            SqlParametros.Value = formu.GetMatGestorFO();
+            SqlParametros = Comando.Parameters.Add("@Dni_cli", SqlDbType.VarChar);
+            SqlParametros.Value = formu.GetDniCliFO();
+            SqlParametros = Comando.Parameters.Add("@Nombre_cony", SqlDbType.VarChar);
+            SqlParametros.Value = formu.GetNombConyuFO();
+            SqlParametros = Comando.Parameters.Add("@Apellido_cony", SqlDbType.VarChar);
+            SqlParametros.Value = formu.GetApeConyuFO();
+            SqlParametros = Comando.Parameters.Add("@Personeria", SqlDbType.VarChar);
+            SqlParametros.Value = formu.GetPersoneriaFO();
+
+        }
+    }
+}
